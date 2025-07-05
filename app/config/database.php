d952b3d51f4353b09f75302a2c0c69ba1d2bc353
buildbysam <sijalmanandhar@kbc.edu.np>
2025-07-05 15:32:45 +0545

    added database config

---
+++ app/config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'file_sharing_app');
+
+$conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if ($conn->connect_error)
+    die("Connection failed: " . $conn->connect_error);
